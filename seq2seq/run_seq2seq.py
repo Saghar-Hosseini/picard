b6064f3801a298f03064d7355e5b646404dda49e
SAghar-Hosseini <saghar.hossaini@gmail.com>
2021-12-07 23:46:10 +0000

    update to docker file

--- seq2seq/run_seq2seq.py
+++ seq2seq/run_seq2seq.py
@@ -47,7 +47,6 @@ def main() -> None:
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
         # let's parse it to get our arguments.
-        pdb.set_trace()
         picard_args, model_args, data_args, data_training_args, training_args = parser.parse_json_file(
             json_file=os.path.abspath(sys.argv[1])
         )
