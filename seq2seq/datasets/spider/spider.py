c5193bea54e69fc9b685cd551fdaebaf71f74b78
SAghar-Hosseini <saghar.hossaini@gmail.com>
2021-12-09 18:33:40 +0000

    trasnlated hi test set

--- seq2seq/datasets/spider/spider.py
+++ seq2seq/datasets/spider/spider.py
@@ -119,7 +119,7 @@ class Spider(datasets.GeneratorBasedBuilder):
             datasets.SplitGenerator(
                 name=datasets.Split.TEST,
                 gen_kwargs={
-                    "data_filepaths": [downloaded_filepath + "/translations/translation_fr_to_en/test.json"],
+                    "data_filepaths": [downloaded_filepath + "/translations/translation_hi_to_en/test.json"],
                     "db_path": downloaded_filepath + "/database",
                 },
             ),            
